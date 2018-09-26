e641e053cc97a8fbacffd806379c620aa19626a9
Ebram <ebram.eid81@gmail.com>
2018-09-26 11:47:19 +0200

    signup Design

--- app/src/main/java/nti/newhorizons/newhorizons/view/activity/signUp/SignUpActivity.java
+++ app/src/main/java/nti/newhorizons/newhorizons/view/activity/signUp/SignUpActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +27,9 @@ public class SignUpActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signup);
 
 
